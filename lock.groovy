79756d5e93ea7e204ba219af0d07931a645a5a9d
Tyler Weaver <tweaver@lgsinnovations.com>
2017-07-10 14:22:52 -0600

    reading only a line a time

--- lock.groovy
+++ lock.groovy
@@ -1,6 +1,7 @@
 /**
- *  Change the Lock
+ *  Airbnb Lock
  *
+ *  Copyright 2017 Tyler Weaver
  *  Copyright 2015 Aaron Parecki
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
@@ -12,11 +13,14 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ * Test URL found on stackoverflow:
+ * https://www.airbnb.it/calendar/ical/2533404.ics?s=580a83c1bcbc0e8af72cfc62bcc2676d
  */
+
 definition(
-    name: "Change the Lock",
-    namespace: "pk.aaron",
-    author: "Aaron Parecki",
+    name: "Airbnb Lock",
+    namespace: "tylerjw",
+    author: "Tyler Weaver",
     description: "Creates an HTTP API to update the codes for a lock",
     category: "",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
@@ -26,7 +30,19 @@ definition(
 
 preferences {
   section("Choose a Door Lock") {
-    input "doorlock", "capability.lockCodes"
+    input "doorlock", "capability.lockCodes", required: true
+    input "ical", "string", required: true, title: "URL to AirBnB Calendar"
+    input "theTime", "time", required: true, title: "Time to check AirBnB Calendar each day"
+    input "firstSlot", "number", required: true, defaultValue: 3, title: "First code slot to control, will use the next slot after this one if there are two guests on the same day"
+  }
+
+  // notifications
+  section("Via a push notification and/or an SMS message"){
+    input("recipients", "contact", title: "Send notifications to") {
+      input "phone", "phone", title: "Enter a phone number to get SMS", required: false
+      paragraph "If outside the US please make sure to enter the proper country code"
+      input "pushAndPhone", "enum", title: "Notify me via Push Notification", required: false, options: ["Yes", "No"]
+    }
   }
 }
 
@@ -40,64 +56,219 @@ def updated() {
   log.debug "Updated with settings: ${settings}"
 
   unsubscribe()
+  unschedule()
   initialize()
 }
 
 def initialize() {
-  subscribe(doorlock, "codeReport", codeReportEvent);
-  subscribe(doorlock, "codeChanged", codeChangedEvent);
+  subscribe(doorlock, "codeReport", codeReportEvent)
+  subscribe(doorlock, "codeChanged", codeChangedEvent)
+
+  schedule(theTime, doCalenderCheck)
 }
 
-mappings {
-  path("/lock/code") {
-    action: [
-      GET: "listCode",
-      POST: "updateCode"
-    ]
+def doCalenderCheck() {
+  def params = [
+    uri: ical
+  ]
+  try {
+    httpGet(params) { resp ->
+      def data = parseICal(resp.data)
+
+      for (event in data) {
+        log.debug "start: ${event['dtStart']}, end: ${event['dtEnd']}, phone: ${event['phone']}"
+        if (!event['phone']) {
+          log.debug "${event['record']}"
+        }
+      }
+    }
+  } catch (e) {
+    log.error "something went wrong: $e"
   }
 }
 
-def listCode() {
-  doorlock.requestCode(params.num.toInteger())
-  return ["num":params.num.toInteger()]
-}
-
-def updateCode() {
-  log.debug "update | request: params: ${params}"
-  doorlock.setCode(params.num.toInteger(), params.code)
-  return ["num":params.num.toInteger(), "code": params.code.toInteger()]
+def setCode(number, code) {
+  doorlock.setCode(number, code)
 }
 
 def codeReportEvent(evt) {
   log.debug "Got the code report event"
   log.debug evt.jsonData
-  /*
-  sendPostRequest([
-    type: "CodeReport",
-    locationId: evt.locationId,
-    locationName: location.name,
-    descriptionText: evt.descriptionText,
-    num: evt.value,
-    code: evt.jsonData.code
-  ])
-  */
-  sendPush(location.name + " door code was set to " + evt.jsonData.code)
+  sendMessage(location.name + " door code was set to " + evt.jsonData.code)
 }
 
 def codeChangedEvent(evt) {
-  log.debug "Code changed"
-  log.debug evt.value
-  /*
-  sendPostRequest([
-    type: "CodeChanged",
-    num: evt.value
-  ])
-  */
+  log.debug "Code changed for slot $evt.value"
 }
 
-def sendPostRequest(data) {
-  httpPostJson([
-    uri: "http://door.launchpad4.com/notify.php",
-    body: data
-  ])
+def sendMessage(msg) {
+  Map options = [translatable: true, triggerEvent: evt]
+
+  if (location.contactBookEnabled) {
+    sendNotificationToContacts(msg, recipients, options)
+  } else {
+    if (phone) {
+      options.phone = phone
+      if (pushAndPhone != 'No') {
+        log.debug 'Sending push and SMS'
+        options.method = 'both'
+      } else {
+        log.debug 'Sending SMS'
+        options.method = 'phone'
+      }
+    } else if (pushAndPhone != 'No') {
+      log.debug 'Sending push'
+      options.method = 'push'
+    } else {
+      log.debug 'Sending nothing'
+      options.method = 'none'
+    }
+    sendNotification(msg, options)
+  }
 }
+
+String toString(ByteArrayInputStream is) {
+  int size = is.available();
+
+  char[] theChars = new char[size];
+  byte[] bytes    = new byte[size];
+
+  is.read(bytes, 0, size);
+  for (int i = 0; i < size;) {
+    theChars[i] = (char)(bytes[i++]&0xff);
+  }
+  
+  return new String(theChars);
+}
+
+String readLine(ByteArrayInputStream is) {
+  int size = is.available();
+  if (size <= 0) {
+    return null;
+  }
+
+  String ret = "";
+  byte data = 0;
+  char ch;
+
+  while (true) {
+    data = is.read();
+    if (data == -1) {
+      // we are done here
+      break;
+    }
+
+    ch = (char)(data&0xff);
+    if (ch == '\n') {
+      break;
+    }
+
+    ret += ch;
+
+    if (ret.endsWith("\\n")) {
+      ret = ret.replaceAll(/\\n/,"");
+      break;
+    }
+  }
+
+  return ret;
+}
+
+def parseICal(ByteArrayInputStream is) {
+  def iCalEvents = []
+  def iCalEvent = null
+  def sincePhone = 100
+
+  while (true) {
+    def line = readLine(is)
+
+    if (line == null) {
+      break;
+    }
+
+    if (line == "BEGIN:VEVENT") {
+      iCalEvent = [record:'']
+    } else if (line == "END:VEVENT") {
+      iCalEvents.push(iCalEvent)
+      iCalEvent = null
+    } else if (iCalEvent != null) {
+      // parse line
+      def compoundKey = null
+      def subKey = null
+      def key = null
+      def value = null
+
+      sincePhone++;
+
+      if ( line ==~ /^[A-Z]+[;:].*/ ) {
+        // grab everything before the :
+        key = line.replaceAll(/:.*/, '')
+        // grab everything before the ;
+        compoundKey = key.replaceAll(/;.*/, '')
+        // grab everything after the ${key}:
+        value = line.replaceFirst(key + ':', '').trim()
+        // grab everything before the ; in the key
+        if (compoundKey != key) {
+          // we found a compound date key
+          subKey = key.replaceFirst(compoundKey + ';', '').trim()
+        }
+
+        if (key == 'DESCRIPTION') {
+          // we found the start of the description
+          key = value.replaceAll(/:.*/, '')
+          value = value.replaceFirst(key + ':', '').trim()
+        }
+
+        if (key == 'UID') { iCalEvent.put('uid',value) }
+        else if (key == 'CREATED') { iCalEvent.put('created', value) }
+        else if (key == 'RRULE') { iCalEvent.put('rRule', value) }
+        else if (key == 'RDATE') { iCalEvent.put('rDate', value) }
+        else if (key == 'DTSTAMP') { iCalEvent.put('dtStamp', parseDate(value)) }
+        else if (key == 'CHECKIN') { iCalEvent.put('checkin', value) }
+        else if (key == 'CHECKOUT') { iCalEvent.put('checkout', value) }
+        else if (key == 'NIGHTS') { iCalEvent.put('nights', value) }
+        else if (key == 'EMAIL') { iCalEvent.put('email', value) }
+        else if (key == 'SUMMARY') { iCalEvent.put('summary', value) }
+        else if (key == 'LOCATION') { iCalEvent.put('location', value) }
+        else if (key == 'PHONE') { sincePhone = 0; }
+        else if (compoundKey == 'DTSTART') {
+          iCalEvent.put('dtStartString', value)
+          iCalEvent.put('dtStart', parseDate(value))
+          iCalEvent.put('dtStartTz', subKey)
+        } else if (compoundKey == 'DTEND') {
+          iCalEvent.put('dtEndString', value)
+          iCalEvent.put('dtEnd', parseDate(value)) 
+          iCalEvent.put('dtEndTz', subKey)
+        }
+      }
+
+      if (sincePhone == 1) {
+        // phone number
+        iCalEvent.put('phone', line)
+      }
+
+      if (line) {
+        iCalEvent['record'] = iCalEvent['record'] + line + '\n'
+      }
+    }
+  }
+  
+
+  return iCalEvents
+}
+
+Date parseDate(String value) {
+  if ( value ==~ /[0-9]*T[0-9]*Z/ ) {
+    Date.parse("yyyyMMdd'T'HHmmss'Z'", value)
+  } else if ( value ==~ /[0-9]*T[0-9]*/ ) {
+    Date.parse("yyyyMMdd'T'HHmmss", value)
+  } else if ( value ==~ /[0-9]*/ ) {
+    Date.parse("yyyyMMdd", value)
+  } else {
+    println "WARNING: unknown date format: ${value}"
+    null
+  }
+}
+
+
+
